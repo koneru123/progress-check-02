7b9a22d75e27ee54b1d1d91e975d7d8b29186961
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-24 09:53:42 -0800

    return max value in the object

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 // For more info on max, see the README.md or underscore docs
-var max = function(list, iteratee) {
-  // YOUR CODE HERE
+let maxValue = function(individualList) {
+  return individualList.age;
 }
+
+var max = function(list, maxValue) {
+  //if Iteratee function is provided
+  let arr = [];
+  let firstIndividualList = maxValue(list[0]);
+  for(let i = 1; i < list.length; i++) {
+    arr = [];
+    if(maxValue(list[i]) > firstIndividualList ) {
+      firstIndividualList =  maxValue(list[i]);
+      arr.push(list[i]);
+    }
+  }
+  return JSON.stringify(arr);
+}
+
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+console.log(max(stooges, function(stooge){ return stooge.age; })); //{name: 'curly', age: 60}
+//var value = {name: 'moe', age: 40};
+//console.log(maxValue(value));
